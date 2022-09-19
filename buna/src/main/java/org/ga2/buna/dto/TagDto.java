b9c5e2c6b498d80c2bc72f8d4da6c56f089b019f
Kim Kyubin <skipperkim307@gmail.com>
2022-09-19 17:53:07 +0900

    feat : 태그 등록 순으로 보여주기 및 다른 태그 추가 구현

---
+++ buna/src/main/java/org/ga2/buna/dto/TagDto.java
@@ -0,0 +1,14 @@
+package org.ga2.buna.dto;
+
+public class TagDto {
+	private String tagName;
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+	
+}
