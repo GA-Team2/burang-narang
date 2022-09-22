ea5fcf7a94354369ce806e0f83c4ae3a34d93428
Byungtae Han <gksqudxo8562@gmail.com>
2022-09-22 10:15:22 +0900

    project : buna delete

--- buna/src/main/webapp/logOut.jsp
+++
@@ -1,6 +0,0 @@
-<%
-	/* 세션값 삭제 */
-	session.invalidate();
-	/* 메인 페이지로 리다이렉트 */
-	response.sendRedirect("index.jsp");
-%>
--- buna/src/main/webapp/script/script_index.js
+++
@@ -1,8 +0,0 @@
-// $(function() unchecked(){
-//     $("Input:radio[name='radio_unchecked']'").prop('checked',false);
-// });
-$(document).ready(function () {
-  $("#radio_unchecked").click(function () {
-    $("input[type=radio][name=gnb]").prop("checked", false);
-  });
-});
