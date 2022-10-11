e9ec92f333158e4c35291b370993eee9846f9a15
Next-Episode <gksqudxo8562@gmail.com>
2022-10-12 00:12:18 +0900

    script : modify

--- src/main/webapp/scripts/script_index.js
+++ src/main/webapp/scripts/script_index.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function () {
+	const content = $("#content");
+
 	/**
 	 * 시간마다 백그라운드를 바꾸는 function
 	 */
@@ -7,13 +9,13 @@ $(document).ready(function () {
 	  var now = new Date();
 	  var hour = now.getHours();
 	  if (hour >= 7 && hour < 10) {
-		  $("#content").addClass("bgSunrise");
+		  content.addClass("bgSunrise");
 	  } else if (hour >= 10 && hour < 16) {
-		  $("#content").addClass("bgAfter");
+		  content.addClass("bgAfter");
 	  } else if (hour >= 16 && hour < 18) {
-		  $("#content").addClass("bgSunset");
+		  content.addClass("bgSunset");
 	  } else {
-		  $("#content").addClass("bgNight");
+		  content.addClass("bgNight");
 	    $("#gnb_traffic h2").css("color", "#eeecc0");
 	    $("#main_menu").css("color", "#eeecc0");
 	  }
@@ -23,9 +25,16 @@ $(document).ready(function () {
 	 * 로그인 시 인트로 애니메이션을 제거하는 function
 	 */
 	$(function(){
+		const left1 = $("#think_left");
+		const left2 = $("#think_left3");
+		const right = $("#think_right");
+		const left1_text = $("#content>label:nth-of-type(1)");
+		const left2_text = $("#content>label:nth-of-type(3)");
+		const right_text = $("#content>label:nth-of-type(2)");
+		const lion = $("#lion_click");
 	  var nick = $("#session_nick").val();
 	  if (nick == "null") {
-	    $("#content").delay(3500).animate(
+		  content.delay(3500).animate(
 	      {
 	        opacity: 1,
 	      },
@@ -33,57 +42,57 @@ $(document).ready(function () {
 	      "linear"
 	    );
 
-	    $("#think_left").delay(2000).addClass("animate__bounceIn");
-	    $("#think_left").delay(2000).fadeIn(100);
-	    $("#content>label:nth-of-type(1)").delay(4100).fadeIn(100);
+		  left1.delay(2000).addClass("animate__bounceIn");
+		  left1.delay(2000).fadeIn(100);
+		  left1_text.delay(4100).fadeIn(100);
 
-	    $("#think_left3").delay(2000).addClass("animate__bounceIn");
-	    $("#think_left3").delay(2000).fadeIn(100);
-	    $("#content>label:nth-of-type(3)").delay(4100).fadeIn(100);
+		  left2.delay(2000).addClass("animate__bounceIn");
+		  left2.delay(2000).fadeIn(100);
+		  left2_text.delay(4100).fadeIn(100);
 
-	    $("#think_right").delay(2000).addClass("animate__bounceIn");
-	    $("#think_right").delay(2000).fadeIn(100);
-	    $("#content>label:nth-of-type(2)").delay(4100).fadeIn(100);
+		  right.delay(2000).addClass("animate__bounceIn");
+		  right.delay(2000).fadeIn(100);
+		  right_text.delay(4100).fadeIn(100);
 	
 	    $("#lion").click(function () {
-	      if ($("#think_left").is(":visible")) {
-	        $("#think_left").delay(100).fadeOut(100);
-	        $("#content>label:nth-of-type(1)").fadeOut(100);
-	        $("#lion_click").fadeIn(100);
+	      if (left1.is(":visible")) {
+			  left1.delay(100).fadeOut(100);
+			  left1_text.fadeOut(100);
+			  lion.fadeIn(100);
 	      } else {
-	        $("#think_left").addClass("animate__bounceIn");
-	        $("#think_left").fadeIn(100);
-	        $("#content>label:nth-of-type(1)").delay(100).fadeIn(100);
-	        $("#lion_click").fadeOut(100);
+			  left1.addClass("animate__bounceIn");
+			  left1.fadeIn(100);
+			  left1_text.delay(100).fadeIn(100);
+			  lion.fadeOut(100);
 	      }
 	
-	      if ($("#think_left3").is(":visible")) {
-	        $("#think_left3").delay(100).fadeOut(100);
-	        $("#content>label:nth-of-type(3)").fadeOut(100);
-	        $("#lion_click").fadeIn(100);
+	      if (left2.is(":visible")) {
+			  left2.delay(100).fadeOut(100);
+			  left2_text.fadeOut(100);
+			  lion.fadeIn(100);
 	      } else {
-	        $("#think_left3").addClass("animate__bounceIn");
-	        $("#think_left3").fadeIn(100);
-	        $("#content>label:nth-of-type(3)").fadeIn(100);
-	        $("#lion_click").fadeOut(100);
+			  left2.addClass("animate__bounceIn");
+			  left2.fadeIn(100);
+			  left2_text.fadeIn(100);
+			  lion.fadeOut(100);
 	      }
 	
-	      if ($("#think_right").is(":visible")) {
-	        $("#think_right").delay(100).fadeOut(100);
-	        $("#content>label:nth-of-type(2)").fadeOut(100);
+	      if (right.is(":visible")) {
+			  right.delay(100).fadeOut(100);
+			  right_text.fadeOut(100);
 	        $("#lion_click").fadeIn(100);
 	      } else {
-	        $("#think_right").addClass("animate__bounceIn");
-	        $("#think_right").fadeIn(100);
-	        $("#content>label:nth-of-type(2)").delay(100).fadeIn(100);
+			  right.addClass("animate__bounceIn");
+			  right.fadeIn(100);
+			  right_text.delay(100).fadeIn(100);
 	        $("#lion_click").fadeOut(100);
 	      }
 	    });
 	  } else {
-	  	$("#content").css("opacity", 1);
-	    $("#think_left").css("display", "block");
-	    $("#think_left3").css("display", "block");
-	    $("#think_right").css("display", "block");
+		  content.css("opacity", 1);
+		  left1.css("display", "block");
+		  left2.css("display", "block");
+		  right.css("display", "block");
 	    $("#main_title")
 	      .css("top", "10%")
 	      .css("left", "12%")
@@ -94,9 +103,9 @@ $(document).ready(function () {
 	    $("#title_blind").css("display", "none");
 	    $("#flight").removeClass("flight_ani");
 	    $("#flight").css("display", "none");
-	    $("#content>label:nth-of-type(1)").css("display", "block");
-	    $("#content>label:nth-of-type(2)").css("display", "block");
-	    $("#content>label:nth-of-type(3)").css("display", "block");
+		  left1_text.css("display", "block");
+		  right_text.css("display", "block");
+		  left2_text.css("display", "block");
 	  }
   });
 
