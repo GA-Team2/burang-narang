de2eccfa3fb163c84a9dd4db5fa6b79f358d23fc
Kim Kyubin <skipperkim307@gmail.com>
2022-10-14 12:27:41 +0900

    Spring 기본 세팅

---
+++ src/main/java/org/ga2/buna/RootConfig.java
@@ -0,0 +1,11 @@
+package org.ga2.buna;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class RootConfig implements WebMvcConfigurer {
+
+}
