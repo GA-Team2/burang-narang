1496d7f902e2108d56161eb73debdc9b5da3b968
Kim Kyubin <skipperkim307@gmail.com>
2022-10-17 16:39:03 +0900

    JSON 설정 추가

--- src/main/java/org/ga2/buna/controller/MakePlanController.java
+++ src/main/java/org/ga2/buna/controller/MakePlanController.java
@@ -5,13 +5,16 @@ import org.ga2.buna.service.PopularTag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/new")
 public class MakePlanController {
     private final PopularTag popularTag;
 
-    @GetMapping("/new")
+    @GetMapping
     public String popularTagList(Model model) {
         model.addAttribute("list", popularTag.findAll());
 
