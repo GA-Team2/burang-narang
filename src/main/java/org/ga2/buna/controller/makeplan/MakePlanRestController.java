68bd60916936c02addefdcece01ae410b70aa2fc
Kim Kyubin <skipperkim307@gmail.com>
2022-10-19 14:50:02 +0900

    설정 변경

--- src/main/java/org/ga2/buna/controller/makeplan/MakePlanRestController.java
+++ src/main/java/org/ga2/buna/controller/makeplan/MakePlanRestController.java
@@ -1,11 +1,14 @@
 package org.ga2.buna.controller.makeplan;
 
 import lombok.AllArgsConstructor;
+import org.ga2.buna.dto.tag.TagDto;
 import org.ga2.buna.service.makeplan.InitAllMapImpl;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -13,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MakePlanRestController {
     private final InitAllMapImpl initAllMap;
 
-    @GetMapping(value = "/1", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String ajaxTest() throws Exception {
+    @GetMapping(value = "/1")
+    public List<TagDto> ajaxTest() throws Exception {
         return initAllMap.get();
     }
 }
