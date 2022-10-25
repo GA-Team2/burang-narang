46a10f68ec630d0bc1e9fce57a644d2a6f2b8727
Next-Episode <gksqudxo8562@gmail.com>
2022-10-25 19:13:44 +0900

    validation

--- src/main/java/org/ga2/buna/dto/memberinfo/MemberDTO.java
+++ src/main/java/org/ga2/buna/dto/memberinfo/MemberDTO.java
@@ -4,14 +4,28 @@ import java.sql.Timestamp;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
 public class MemberDTO {
+
+	@NotEmpty
+	@Size(min = 1, max = 6)
 	private String memberNickname;
+	@NotEmpty
 	private String memberPassword;
+	@NotEmpty
+	@Min(1922)
+	@Max(2022)
 	private int memberBirthyear;
+	@NotEmpty
 	private int memberGender;
+	@NotEmpty
 	private Timestamp memberJoindate;
 }
