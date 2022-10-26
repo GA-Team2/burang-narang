464787ca768e0a6ba0cc9499dc62ba710769543d
Byungtae Han <gksqudxo8562@gmail.com>
2022-10-26 16:29:16 +0900

    validation

--- src/main/java/org/ga2/buna/controller/signup/SignUpController.java
+++ src/main/java/org/ga2/buna/controller/signup/SignUpController.java
@@ -1,27 +1,43 @@
 package org.ga2.buna.controller.signup;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.ga2.buna.dto.memberinfo.MemberDTO;
 import org.ga2.buna.service.signup.SignUp;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+/**
+ * 회원가입 Controller
+ */
 @Controller
 @RequestMapping("/signup")
 @AllArgsConstructor
+@Slf4j
 public class SignUpController {
+
+    //회원가입 서비스 계층 필드(생성자로 의존주입)
     private final SignUp signUp;
 
+    /**
+     * 회원가입 form 및 validation 유효성 검사
+     * @param dto 회원정보를 넣을 dto
+     * @param result validation 검사의 결과값을 저장한 객체
+     * @return validation 검사결과 error가 출력되면 회원가입 form으로 리턴. error가 없을 경우 로그인 페이지로 리턴
+     */
     @RequestMapping("/ok")
-    public String signup(HttpServletRequest request, @Valid MemberDTO dto) {
-        dto.setMemberNickname(request.getParameter("memberNickname"));
-        dto.setMemberPassword(request.getParameter("memberPassword"));
-        dto.setMemberBirthyear(Integer.parseInt(request.getParameter("memberBirthyear")));
-        dto.setMemberGender(Integer.parseInt(request.getParameter("memberGender")));
-        signUp.insertMember(dto);
-        return "redirect:/login";
+    public String signup(@ModelAttribute @Valid MemberDTO dto, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println(result);
+            return "redirect:/signup";
+        } else {
+            System.out.println(dto.getMemberNickname());
+            signUp.insertMember(dto);
+            return "redirect:/login";
+        }
     }
 }
--- src/main/java/org/ga2/buna/dto/memberinfo/MemberDTO.java
+++ src/main/java/org/ga2/buna/dto/memberinfo/MemberDTO.java
@@ -7,31 +7,35 @@ import lombok.Setter;
 
 import javax.validation.constraints.*;
 
-
+/**
+ * 회원정보 DTO
+ * @author 한병태 validtion 추가
+ */
 @Getter
 @Setter
 public class MemberDTO {
 
-	@NotEmpty
+	//Null 비허용, size(1~12)
+	@NotNull
 	@Size(min = 1, max = 6)
 	private String memberNickname;
 
-	@NotEmpty
-	@Pattern(regexp = "[0-9]")
-	@Pattern(regexp = "[a-zA-Z]")
-	@Pattern(regexp = "[~!@\\#$%<>^&*]")
+	//Null 비허용, password pattern 숫자, 문자, 특수문자 포함, size(8~49)
+	@NotNull
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")
 	@Size(min = 8, max = 49)
 	private String memberPassword;
 
-	@NotEmpty
+	//Null 비허용, 최소 숫자 1922, 최대 숫자 2022
+	@NotNull
 	@Min(1922)
 	@Max(2022)
-	@PositiveOrZero
 	private int memberBirthyear;
 
-	@NotEmpty
+	//Null 비허용
+	@NotNull
 	private int memberGender;
 
-	@NotEmpty
+	//입력값이 아닌 시스템 자체에서 설정되는 회원정보는 validation 제외
 	private Timestamp memberJoindate;
 }
