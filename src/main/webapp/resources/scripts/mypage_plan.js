a31f5b51d7f89dfa26cac525426934d30359d877
heejeong <heejeong1115@gmail.com>
2022-10-26 16:34:45 +0900

    mypage ajax

--- src/main/webapp/resources/scripts/mypage_plan.js
+++ src/main/webapp/resources/scripts/mypage_plan.js
@@ -30,35 +30,39 @@ function delete_ok(rownum, i) {
 /* 공개/비공개 알림창 */
 function sharecheck(shared, rownum, i) {
     let result;
+    console.log(shared);
     if (shared == 1) {
         result = confirm("확인버튼 클릭 시 일정이 비공개됩니다.");
         if (result == true) {
+            alert("플랜이 비공개되었습니다.");
             shareajax(shared, rownum, i);
-            // location.href = "/mypage/share?rownum=" + rownum + "&shared=" + shared;
         }
     } else {
         result = confirm("확인버튼 클릭 시 일정이 공개됩니다.");
         if (result == true) {
+            alert("플랜이 공개되었습니다.");
             shareajax(shared, rownum, i);
-            // location.href = "/mypage/share?rownum=" + rownum + "&shared=" + shared;
         }
     }
 }
 
+/* 플랜 공개/비공개 ajax */
 function shareajax(shared, rownum, i) {
 // XMLHttpRequest 객체 생성
     const xhr = new XMLHttpRequest();
 // HTTP 요청 초기화
-    xhr.open('GET', "/mypage/share?rownum=" + rownum + "shared = " + shared);
+    xhr.open('GET', "/mypage/share?rownum=" + rownum + "&shared=" + shared);
 // HTTP 요청 전송
     xhr.send();
 // load 이벤트는 HTTP 요청이 성공적으로 완료된 경우 발생
     xhr.onload = () => {
-        if (xhr.status === 200) {
-            alert("플랜이 삭제되었습니다.");
-            document.getElementById("plan"+i).remove();
+        const re = xhr.response;
+        if (xhr.status === 200 && re == 1) {
+            document.getElementById("plan"+i+"share").value = '일정 비공개';
+        } else if (xhr.status === 200 && re == 0) {
+            document.getElementById("plan"+i+"share").value = '일정 공개';
         } else {
-            alert("삭제 실패");
+            alert("통신 실패");
             console.error('Error', xhr.status, xhr.statusText);
         }
     }
