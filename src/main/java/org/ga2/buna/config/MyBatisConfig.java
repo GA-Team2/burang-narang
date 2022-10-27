a8f1bb8c711321104342ea0b23188b79692883ae
Kim Kyubin <skipperkim307@gmail.com>
2022-10-27 16:45:07 +0900

    MyBatis 전환 완료

--- src/main/java/org/ga2/buna/config/MyBatisConfig.java
+++ src/main/java/org/ga2/buna/config/MyBatisConfig.java
@@ -1,19 +1,28 @@
 package org.ga2.buna.config;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.ga2.buna.dao.plandetail.PlanDetailDAO;
+import org.ga2.buna.dao.plandetail.PlanDetailMapper;
 import org.ga2.buna.dao.tag.PopularTagMapper;
 import org.ga2.buna.dao.tag.TagDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MyBatisConfig {
 
     private final PopularTagMapper popularTagMapper;
+    private final PlanDetailMapper planDetailMapper;
 
     @Bean
     public TagDao tagDao() {
         return new TagDao(popularTagMapper);
+    }
+
+    @Bean
+    public PlanDetailDAO planDetailDAO() {
+        return new PlanDetailDAO(planDetailMapper);
     }
 }
--- src/main/java/org/ga2/buna/dto/tag/TagDto.java
+++ src/main/java/org/ga2/buna/dto/tag/TagDto.java
@@ -9,6 +9,6 @@ import lombok.Setter;
  */
 @Getter @Setter
 public class TagDto {
-	private String tName;
+	private String tagName;
 	private int tagHit;
 }
