483875814883d0a6e612d4445f82286ab2b3ce45
Kim Kyubin <skipperkim307@gmail.com>
2022-10-29 21:47:22 +0900

    정리

--- src/main/java/org/ga2/buna/BunaApplication.java
+++ src/main/java/org/ga2/buna/BunaApplication.java
@@ -2,7 +2,6 @@ package org.ga2.buna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class BunaApplication {
