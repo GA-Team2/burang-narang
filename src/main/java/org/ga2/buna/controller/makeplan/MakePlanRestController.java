fd84dd53e45eabec483896a4d004fb9bb698bcee
Kim Kyubin <skipperkim307@gmail.com>
2022-10-29 16:02:04 +0900

    전체 포맷팅

--- src/main/java/org/ga2/buna/controller/makeplan/MakePlanRestController.java
+++ src/main/java/org/ga2/buna/controller/makeplan/MakePlanRestController.java
@@ -3,10 +3,12 @@ package org.ga2.buna.controller.makeplan;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ga2.buna.dao.planinfo.PlanInfoRepository;
-import org.ga2.buna.service.makeplan.SpotData;
 import org.ga2.buna.dto.plandetail.PlanDetailDTO;
 import org.ga2.buna.dto.planinfo.PlanInfoDTO;
-import org.ga2.buna.service.makeplan.*;
+import org.ga2.buna.service.makeplan.SavePlanDetail;
+import org.ga2.buna.service.makeplan.SavePlanInfo;
+import org.ga2.buna.service.makeplan.SaveTagList;
+import org.ga2.buna.service.makeplan.SpotData;
 import org.ga2.buna.service.spot.Spot;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,11 +27,11 @@ public class MakePlanRestController {
     private final PlanInfoRepository planInfoRepository;
 
     /*
-    * 장소 종류 받아와 spotList에 띄울 데이터 보내는 메서드
-    *
-    * @param kindOfSpot 장소 종류
-    * @return 장소 데이터 리스트
-    * */
+     * 장소 종류 받아와 spotList에 띄울 데이터 보내는 메서드
+     *
+     * @param kindOfSpot 장소 종류
+     * @return 장소 데이터 리스트
+     * */
 
     @GetMapping(value = "/spot")
     public List<Spot> getSpotList(String kindOfSpot) {
@@ -52,7 +54,7 @@ public class MakePlanRestController {
      *
      * @param planInfoDTO 플랜 인포 객체
      * */
-    @RequestMapping(value = "/planinfo", method = { RequestMethod.POST })
+    @RequestMapping(value = "/planinfo", method = {RequestMethod.POST})
     public void getFormData(@RequestBody PlanInfoDTO planInfoDTO) {
         log.info("title={}, firstDate={}, lastDate={}, tagList={}"
                 , planInfoDTO.getPlanTitle()
@@ -70,13 +72,13 @@ public class MakePlanRestController {
      * @param planDetailDTOList 플랜 디테일 객체 리스트
      * @return created === 200 게시물 번호 반환
      * */
-    @RequestMapping(value = "/plandetail", method = { RequestMethod.POST })
+    @RequestMapping(value = "/plandetail", method = {RequestMethod.POST})
     public int getFormData(@RequestBody List<PlanDetailDTO> planDetailDTOList) {
         log.info("tripday={}, tripdate={}, spotname={}, spotnumber={}"
-                    , planDetailDTOList.get(0).getPlanTripDay()
-                    , planDetailDTOList.get(0).getPlanTripDate()
-                    , planDetailDTOList.get(0).getPlanSpotName()
-                    , planDetailDTOList.get(0).getSpotSerialNumber());
+                , planDetailDTOList.get(0).getPlanTripDay()
+                , planDetailDTOList.get(0).getPlanTripDate()
+                , planDetailDTOList.get(0).getPlanSpotName()
+                , planDetailDTOList.get(0).getSpotSerialNumber());
 
         savePlanDetail.saveAll(planDetailDTOList);
 
