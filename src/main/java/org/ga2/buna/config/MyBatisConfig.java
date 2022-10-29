755588912972a6636cf1e38270f3474ee12d155f
SeungGu Kang <aodnsrhf@naver.com>
2022-10-29 14:39:46 +0900

    dao 이름 변경

--- src/main/java/org/ga2/buna/config/MyBatisConfig.java
+++ src/main/java/org/ga2/buna/config/MyBatisConfig.java
@@ -7,16 +7,16 @@ import org.ga2.buna.dao.dateCheck.DateCheckDAO;
 import org.ga2.buna.dao.dateCheck.DateCheckMapper;
 import org.ga2.buna.dao.login.LoginDAO;
 import org.ga2.buna.dao.login.LoginMapper;
-import org.ga2.buna.dao.planboard.PopDAO;
-import org.ga2.buna.dao.planboard.PopMapper;
-import org.ga2.buna.dao.plandetail.PlanDetailDAO;
-import org.ga2.buna.dao.plandetail.PlanDetailMapper;
-import org.ga2.buna.dao.signup.SignUpDAO;
-import org.ga2.buna.dao.signup.SignUpMapper;
 import org.ga2.buna.dao.memberinfo.MemberDAO;
 import org.ga2.buna.dao.memberinfo.MemberInfoMapper;
+import org.ga2.buna.dao.planboard.PopMapper;
+import org.ga2.buna.dao.planboard.PopRepository;
+import org.ga2.buna.dao.plandetail.PlanDetailDAO;
+import org.ga2.buna.dao.plandetail.PlanDetailMapper;
 import org.ga2.buna.dao.planinfo.PlanInfoDAO;
 import org.ga2.buna.dao.planinfo.PlanInfoMapper;
+import org.ga2.buna.dao.signup.SignUpDAO;
+import org.ga2.buna.dao.signup.SignUpMapper;
 import org.ga2.buna.dao.tag.TagMapper;
 import org.ga2.buna.dao.tag.TagRepository;
 import org.springframework.context.annotation.Bean;
@@ -57,8 +57,8 @@ public class MyBatisConfig {
     }
 
     @Bean
-    public PopDAO popDAO() {
-        return new PopDAO(popMapper);
+    public PopRepository popRepository() {
+        return new PopRepository(popMapper);
     }
 
     @Bean
