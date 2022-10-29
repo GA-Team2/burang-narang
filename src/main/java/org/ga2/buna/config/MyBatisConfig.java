0872e5ded0a99f5becf5ac2a81920a8a7b29c371
Kim Kyubin <skipperkim307@gmail.com>
2022-10-29 14:23:09 +0900

    DAO -> Repository 이름 변경

--- src/main/java/org/ga2/buna/config/MyBatisConfig.java
+++ src/main/java/org/ga2/buna/config/MyBatisConfig.java
@@ -18,7 +18,7 @@ import org.ga2.buna.dao.memberinfo.MemberInfoMapper;
 import org.ga2.buna.dao.planinfo.PlanInfoDAO;
 import org.ga2.buna.dao.planinfo.PlanInfoMapper;
 import org.ga2.buna.dao.tag.TagMapper;
-import org.ga2.buna.dao.tag.TagDao;
+import org.ga2.buna.dao.tag.TagRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,8 +37,8 @@ public class MyBatisConfig {
     private final SignUpMapper signUpMapper;
 
     @Bean
-    public TagDao tagDao() {
-        return new TagDao(tagMapper);
+    public TagRepository tagDao() {
+        return new TagRepository(tagMapper);
     }
 
     @Bean
