e82971ffdf1ab13bb5d3de4f1ca306a35af44a17
Kim Kyubin <skipperkim307@gmail.com>
2022-10-29 21:45:59 +0900

    불필요한 클래스 삭제

--- src/main/java/org/ga2/buna/BunaApplication.java
+++ src/main/java/org/ga2/buna/BunaApplication.java
@@ -1,16 +1,10 @@
 package org.ga2.buna;
 
-import org.ga2.buna.config.MyBatisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
-@Import(MyBatisConfig.class)
 public class BunaApplication {
 
 	public static void main(String[] args) {
