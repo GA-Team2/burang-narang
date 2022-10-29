6846dd7a0509c7cafd3039e1b676f75089485202
SeungGu Kang <aodnsrhf@naver.com>
2022-10-29 14:36:46 +0900

    dao -> repository

--- src/main/java/org/ga2/buna/service/popularityplan/PlanTop30List.java
+++ src/main/java/org/ga2/buna/service/popularityplan/PlanTop30List.java
@@ -1,7 +1,7 @@
 package org.ga2.buna.service.popularityplan;
 
 import lombok.RequiredArgsConstructor;
-import org.ga2.buna.dao.planboard.PopDAO;
+import org.ga2.buna.dao.planboard.PopRepository;
 import org.ga2.buna.dto.planboard.PopDTO;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +11,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlanTop30List implements PlanTop30 {
 
-    private final PopDAO popDAO;
+    private final PopRepository popRepository;
 
     @Override
     public List<PopDTO> find30() {
-        return popDAO.top30();
+        return popRepository.top30();
     }
 }
